3e7c9656fe92b2fe306d4912d22aa4ae01903646
Ilya Lokhmanov <karn23@mail.ru>
2025-03-09 20:01:06 +0300

    Добавление функции пересоздании всех таблиц

--- server/database.py
+++ server/database.py
@@ -2,9 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from models.models_test import Base
 
-# localhost URL
-#DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/report_system"
-#DATABASE_URL = "postgresql+asyncpg://admin:admin@localhost/report_system"
+
 
 # Docker URL
 DATABASE_URL="postgresql+asyncpg://admin:admin@automated-report-system-db-1:5432/report_system" # docker 
@@ -14,6 +12,11 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
+
+async def recreate_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)  
+        await conn.run_sync(Base.metadata.create_all)  
 
 async def init_db():
     async with engine.begin() as conn:
